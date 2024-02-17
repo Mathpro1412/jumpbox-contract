ce20e827c98fc959b7a994c23dd8bbedde77ee18
Anon Jansaichon <a.jansaichon@gmail.com>
2024-02-17 11:39:49 +0700

    add: init node project

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Simple endpoint
+app.get("/greeting", (req, res) => {
+  const name = req.query.name || "World";
+  res.json({ message: `Hello, ${name}!` });
+});
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
