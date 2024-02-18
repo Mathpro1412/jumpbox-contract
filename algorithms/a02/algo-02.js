2e4e21ecdbcd61982d6d0b6c38fb6ad91839f202
Anon Jansaichon <a.jansaichon@gmail.com>
2024-02-18 15:11:36 +0700

    fix: algo-02.js

--- algorithms/a02/algo-02.js
+++ algorithms/a02/algo-02.js
@@ -1,14 +1,31 @@
-function distribute(node, workload) {
-    let arr = []
-    let node_arr = []
-    if (workload % node == 0) {
-        for (let i = 0; i < workload; i++) {
-            if(node_arr.length < node){
-                node_arr.push(i)
-            }
+function distributeWorkload(numServers, workload) {
+    const totalJobs = workload.reduce((acc, cur) => acc + cur, 0);
+    const avgJobsPerServer = Math.floor(totalJobs / numServers);
+    const remainingJobs = totalJobs % numServers;
+
+    const distribution = [];
+    let currentServer = 0;
+    let currentJobIndex = 0;
+
+    for (let i = 0; i < numServers; i++) {
+        let serverLoad = avgJobsPerServer;
+        if (i < remainingJobs) {
+            serverLoad++;
         }
+
+        const serverJobs = [];
+        for (let j = 0; j < serverLoad; j++) {
+            serverJobs.push(currentJobIndex++);
+        }
+
+        distribution.push(serverJobs);
     }
-    return node_arr
+
+    return distribution;
 }
 
-console.log(distribute(2, 4))
+
+const numServers = 3;
+const workload = [3];
+const distribution = distributeWorkload(numServers, workload);
+console.log("Jobs distribution:", distribution);
